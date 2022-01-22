fe30a558a8d6ff6e1048d6f3e36eac206bb013db
cahyeonc <cahyeonc@naver.com>
2022-01-22 22:09:58 +0900

    ask update

--- ask/urls.py
+++ ask/urls.py
@@ -5,4 +5,7 @@ app_name = 'ask'
 
 urlpatterns = [
     path('', views.ask, name= 'ask'),
+    path('<int:pk>/',views.posting, name="posting"),
+    path('new_post/', views.new_post),
+    path('<int:pk>/remove/', views.remove_post),
 ]
--- ask/views.py
+++ ask/views.py
@@ -1,6 +1,29 @@
 import imp
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .models import Post
 
 def ask(request):
-    return HttpResponse('<u>Hello</u>')
+    postlist = Post.objects.all()
+    return render(request, 'ask/question.html', {'postlist':postlist})
+
+def posting(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'ask/posting.html', {'post':post})
+
+def new_post(request):
+    if request.method == 'POST':
+        new_article=Post.objects.create(
+            question=request.POST['question'],
+            contents=request.POST['contents'],
+        )
+        new_article.save()
+        return redirect('/ask/')
+    return render(request, 'ask/new_post.html')
+
+def remove_post(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('/ask/')
+    return render(request, 'ask/remove_post.html', {'Post': post})

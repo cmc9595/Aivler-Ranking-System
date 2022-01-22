89c927da950b996e32ead3eea10e8cf5eeed84f0
cahyeonc <cahyeonc@naver.com>
2022-01-22 15:40:04 +0900

    home-free 연결

--- bbs/urls.py
+++ bbs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'bbs'
 urlpatterns = [
-    path('', views.free),
+    path('', views.free, name= 'free'),
     path('<int:pk>/',views.posting, name="posting"),
     path('new_post/', views.new_post),
     path('<int:pk>/remove/', views.remove_post),

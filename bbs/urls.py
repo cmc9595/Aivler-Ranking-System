258ec1cbd59bb509aca1d60cdd79a8a080d8e85f
cahyeonc <cahyeonc@naver.com>
2022-01-25 11:07:33 +0900

    .

--- bbs/urls.py
+++ bbs/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
 
     path('comment/create/<int:post_id>/', views.comment_create,name='comment_create'), # 댓글 작성
     path('<int:post_id>/<int:comment_id>/',views.comment_delete, name="comment_delete"),
-    
+    path('download/', views.download, name='download'),
 ]+ static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT)
--- bbs/views.py
+++ bbs/views.py
@@ -46,6 +46,24 @@ def new_post(request):
         form = UploadFileForm()
     return render(request, 'bbs/new_post.html')
 
+from config import settings
+import os
+from .models import UploadFile
+def download(request):
+    id = request.GET.get('id')
+    uploadFile = UploadFile.objects.get(id=id)
+    
+    filepath = str(settings.BASE_DIR) + ('/media/%s' % uploadFile.file.name)
+    filename = os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/octet-stream')
+        response['Content-Disposition'] = 'attachment; filename=%s' % filename
+        return response
+
+
+
+
+
 def remove_post(request, post_id):
     post = Post.objects.get(pk=post_id)
     if request.method == 'POST':

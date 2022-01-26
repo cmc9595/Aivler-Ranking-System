cbe1fd167c0649c1c8c520d1059b6754190bdc7c
cahyeonc <cahyeonc@naver.com>
2022-01-26 16:13:32 +0900

    .

--- bbs/urls.py
+++ bbs/urls.py
@@ -6,14 +6,15 @@ from django.conf.urls.static import static
 
 app_name = 'bbs'
 urlpatterns = [
-    path('', views.free, name= 'free'),
-    path('<int:post_id>/',views.posting, name="posting"),
-    path('new_post/', views.new_post),
-    path('<int:post_id>/remove/', views.remove_post),
+    path('', views.free, name= 'free'), # 전체 게시글
+    path('<int:post_id>/',views.posting, name="posting"), # 게시글
+    path('new_post/', views.new_post), # 게시글 새로 작성
+    path('<int:post_id>/remove/', views.remove_post), # 게시글 삭제
 
     path('comment/create/<int:post_id>/', views.comment_create,name='comment_create'), # 댓글 작성
-    path('<int:post_id>/<int:comment_id>/',views.comment_delete, name="comment_delete"),
-    path('<int:uploadfile_id>/download/', views.download, name='download'),
+    path('<int:post_id>/<int:comment_id>/',views.comment_delete, name="comment_delete"), # 댓글 삭제
+    
+    path('<int:uploadfile_id>/download/', views.download, name='download'), # 파일 다운로드
 
 ]+ static(
     settings.MEDIA_URL,
